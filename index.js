85550f5fe135e97c4e7fc209e7e29bab0c98aca5
Swagat <chouguleswagat108@gmail.com>
2026-01-02 13:31:38 +0530

    Add the modified files

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Start of the backend project which is the project management system");
+import dotenv from "dotenv"
+dotenv.config({
+    path : "./.env",
+});
+
+let UserName = process.env.username;
+let ProjectName = process.env.projectname;
+
+console.log("Username: ",UserName);
+console.log("Projectname: ",ProjectName);
+
+console.log("Start of the backend project");
