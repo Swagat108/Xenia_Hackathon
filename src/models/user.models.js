81000c9af77c6357cdcf6e15268a105cb5797ed0
Swagat108 <chouguleswagat108@gmail.com>
2026-02-14 18:55:46 +0530

    Update user.models.js

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -71,10 +71,9 @@ userSchema.pre("save",async function(){
 
     if(!this.isModified("password"))
     {
-        return next();  
+        return;  
     }
     this.password = await bcrypt.hash(this.password,10);
-    next();
 })
 userSchema.methods.isPasswordCorrect = async function (password) 
 {
